03d9c771baafe241805538a58cc018d60fafbf36
Alex Bennée <alex.bennee@linaro.org>
2014-07-21 09:08:52 +0100

    qemu-log: dfilter-ise exec, out_asm, and op_opt
    This ensures the code generation debug code will honour -dfilter if set.
    For the "exec" tracing I've added a new inline macro for efficiency's
    sake. I've not touched CPU_LOG_TB_OP as this is buried in each
    individual target.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    
    ----
    
    v2
       - checkpatch updates
       - add qemu_log_mask_and_addr macro for inline dump for traces
       - re-base on re-factored tcg layout
       - include new Trace & Link lines

--- tcg/tcg.c
+++ tcg/tcg.c
@@ -2305,7 +2305,8 @@ static inline int tcg_gen_code_common(TCGContext *s, TranslationBlock *tb,
      g_assert(tb->tc_size == 0 || search_pc > 0);
 
 #ifdef DEBUG_DISAS
-    if (unlikely(qemu_loglevel_mask(CPU_LOG_TB_OP))) {
+    if (unlikely(qemu_loglevel_mask(CPU_LOG_TB_OP)
+                 && qemu_log_in_addr_range(tb->pc))) {
         qemu_log("OP:\n");
         tcg_dump_ops(s);
         qemu_log("\n");
@@ -2332,7 +2333,8 @@ static inline int tcg_gen_code_common(TCGContext *s, TranslationBlock *tb,
 #endif
 
 #ifdef DEBUG_DISAS
-    if (unlikely(qemu_loglevel_mask(CPU_LOG_TB_OP_OPT))) {
+    if (unlikely(qemu_loglevel_mask(CPU_LOG_TB_OP_OPT)
+                 && qemu_log_in_addr_range(tb->pc))) {
         qemu_log("OP after optimization and liveness analysis:\n");
         tcg_dump_ops(s);
         qemu_log("\n");
